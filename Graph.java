fd578b102dfe0ce36b6c0aebc9fd6975fa31ad5a
Edson-Luiz <90424424+Edson-Luiz@users.noreply.github.com>
2022-08-11 01:10:05 +0000

    Add methods

--- Graph.java
+++ Graph.java
@@ -1,8 +1,15 @@
 public class Graph {
 
-  private int countNodes;
-  private int countEdges;
+  private int countNodes; //Contador de nos
+  private int countEdges; //Contador de arestas
   private int[][] adjMatrix;
+
+  public Graph(int countNodes) {
+
+    this.countNodes = countNodes;
+    this.adjMatrix = new int[countNodes][countNodes];
+
+  }
 
   public int getCountEdges() {
     return countEdges;
@@ -16,50 +23,37 @@ public class Graph {
     return adjMatrix;
   }
 
-  public Graph(int countNodes) {
+  public void addEdge(int source, int sink, int weight) {
 
-    this.countNodes = countNodes;
-    this.adjMatrix = new int[countNodes][countNodes];
+    if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
 
-  }
+      System.out.print("\nInvalid edge: " + source + sink + weight + "\n");
+      return;
+    }
 
-  public void addEdge(int source, int sink, int weight){
-    
-    if(source < 0 || source >= this.countNodes - 1 
-       || sink < 0 || sink > this.countNodes - 1 || weight <=0){
-      
-        System.out.print("\nInvalid edge: " + source + sink + weight);
-        return;
-      }
-    
-      this.adjMatrix[source][sink] = weight;
-      this.countEdges++;
+    this.adjMatrix[source][sink] = weight;
+    this.countEdges++;
 
   }
 
-  public int degree(int node){
+  public int degree(int node) {
 
     int cont = 0;
 
-    if(node > this.countNodes - 1){
+    if (node > this.countNodes - 1) {
       System.err.println("Invalid node: \n");
-    }
-    else 
-    {
+    } else {
       for (int i = 0; i < this.adjMatrix[node].length; i++) {
-      if(this.adjMatrix[node][i] != 0){
+        if (this.adjMatrix[node][i] != 0) {
           cont++;
         }
-      
+
       }
-    
+
     }
-    
+
     return cont;
   }
-
-
-  
 
   public String toString() {
     String str = "";
@@ -73,5 +67,53 @@ public class Graph {
     }
     return str;
   }
+
+  public int highestDegree(){
+    int highest = 0;
+    
+    for (int i = 0; i < this.adjMatrix.length; i++) {
+      int degreeNodeI = this.degree(i);
+        if(degreeNodeI > highest){
+          highest = degreeNodeI;
+      }
+        
+    }
+    
+    return highest;
+  }
+
+  public int lowestDegree(){
+    int lowest = this.adjMatrix.length;
+    
+    for (int i = 0; i < this.adjMatrix.length; i++) {
+      int degreeNodeI = this.degree(i);
+        if(degreeNodeI < lowest){
+          lowest = degreeNodeI;
+      }
+        
+    }
+    
+    return lowest;
+  }
+
+  public Graph complement(){
+    Graph g2 = new Graph(this.countNodes); 
+
+    for (int i = 0; i < this.adjMatrix.length; i++) {
+      for (int j = 0; j < this.adjMatrix[i].length; j++) {
+        if(this.adjMatrix[i][j] == 0 && i != j){
+          g2.addEdge(i, j, 1);
+          
+        }
+        
+      }
+      
+    }
+
+    return g2;
+  }
+
+
+
 
 }
--- Main.java
+++ Main.java
@@ -3,16 +3,20 @@ class Main {
 
     Graph g1 = new Graph(4);
 
-    g1.addEdge(0, 1, 3);
-    g1.addEdge(1, 0, 3);
-    g1.addEdge(0, 3, 4);
-    g1.addEdge(3, 0, 4); 
-    g1.addEdge(3, 4, 4); //Warning
+    g1.addEdge(0, 1, 1);
+    g1.addEdge(1, 0, 1);
+    g1.addEdge(0, 3, 1);
+    g1.addEdge(3, 0, 1);
+    //g1.addEdge(3, 4, 1); // Warning
 
     System.err.println(g1);
-    System.err.println(g1.degree(0));
-    
-    //Criar projeto GitHub e postar no Moodle 
+    // System.err.println(g1.degree(0));
+
+    //System.out.println(g1.highestDegree());
+    //System.out.println(g1.lowestDegree());
+    System.out.println(g1.complement());
+
+    //Postar link atualizado do GIT
     
   }
 
